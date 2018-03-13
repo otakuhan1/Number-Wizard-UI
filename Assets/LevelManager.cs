42cd4668f8a4f37e5c19db454057bd57bf02dd31
otakuhan1 <otakuhan1@yahoo.com>
2018-03-13 00:41:19 -0400

    put Number Wizard UI onto Git

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("level loaded requested for "+name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Game Quited");
+		Application.Quit();
+	}
+	
+}
